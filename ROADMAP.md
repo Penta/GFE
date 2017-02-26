d6f5963cf6817c166179a1713658741912b35680
Penta Pingouin <vgr.penta@yahoo.fr>
2017-02-27 00:46:49 +0100

    Amélioration du script Powershell

--- ROADMAP.md
+++ ROADMAP.md
@@ -5,7 +5,8 @@ Pour la version 1.0 du Gestionnaire de Fond d'Écran, voici les fonctionalités 
  - Création d'une option de recherche ou de filtrage (le filtrage par extension est déjà un début).
  - Faire un historique intélligent des dossiers parcourus.
  - Restorer la première fenêtre si le processus est réouvert une seconde fois.
- - Mise en place d'une fenêtre d'attente lors du chargement de la liste des fichiers
+ - Mise en place d'une fenêtre d'attente lors du chargement de la liste des fichiers.
+ - Pouvoir choisir son exécutable graphiquement dans la fenêtre de configuration pour l'édition des fonds d'écran.
 	
 Idée pouvant venir le jour un jour, mais n'étant pas des prioritées :
  - Mise en place d'un système de diaporamas qui a pour but de remplacer celui par défaut de windows. Il changera le fond d'écranautomatiquement, il sera configurable, et aura les avantages qui sont déjà présent dans le logiciel
--- zipsrc.ps1
+++ zipsrc.ps1
@@ -11,42 +11,58 @@
 #                                       #
 #########################################
 
-$tmp = ".\tmp\"
-$output = ".\Resources\source.zip"
+
+
+# Quelques variables utilisées dans le script
+$dossierCourant = (Get-Item -Path ".\" -Verbose).FullName
+$tmp            = "$dossierCourant\tmp\"
+$output         = "$dossierCourant\Resources\source.zip"
 
 echo "Copie des sources..."
 
 # On supprime les fichiers/dossiers anciens
-If (Test-Path $tmp   ) { Remove-Item $tmp -Recurse    }
-if (Test-Path $output) { Remove-Item $output -Recurse }
+If (Test-Path $tmp   ) { Remove-Item $tmp    -Recurse }
+If (Test-Path $output) { Remove-Item $output -Recurse }
 
 # On créé le dossier temporaire
-New-Item -ItemType Directory -Force -Path $tmp >> null
-Remove-Item null
+New-Item -ItemType Directory -Force -Path $tmp >> $null
 
-Copy-Item "*.csproj" $tmp
-Copy-Item "*sln" $tmp
-Copy-Item "zipsrc.ps1" $tmp
-Copy-Item "app.config" $tmp
-Copy-Item "cleanup.cmd" $tmp
-Copy-Item "*sln" $tmp
-Copy-Item "*.cs" $tmp
-Copy-Item "*.resx" $tmp
-Copy-Item "LICENSE" $tmp
+# On teste si les fichiers existent, et on les copie
+If ( Test-Path "*.csproj"    ) { Copy-Item "*.csproj"    $tmp }
+If ( Test-Path "*.sln"       ) { Copy-Item "*.sln"       $tmp }
+If ( Test-Path "*.cs"        ) { Copy-Item "*.cs"        $tmp }
+If ( Test-Path "*.resx"      ) { Copy-Item "*.resx"      $tmp }
+If ( Test-Path "*.md"        ) { Copy-Item "*.md"        $tmp }
+If ( Test-Path "zipsrc.ps1"  ) { Copy-Item "zipsrc.ps1"  $tmp }
+If ( Test-Path "app.config"  ) { Copy-Item "app.config"  $tmp }
+If ( Test-Path "cleanup.cmd" ) { Copy-Item "cleanup.cmd" $tmp }
+If ( Test-Path "LICENSE"     ) { Copy-Item "LICENSE"     $tmp }
 
-Copy-Item "Properties/" $tmp -Recurse
-Copy-Item "Resources/" $tmp -Recurse
+# On fait pareil, mais pour les dossiers
+If ( Test-Path "Properties/" ) { Copy-Item "Properties/" $tmp -Recurse }
+If ( Test-Path "Resources/"  ) { Copy-Item "Resources/"  $tmp -Recurse }
 
 # Nettoyage des sources
-cd ".\tmp\"
-cmd /c ".\cleanup.cmd"
-cd ..
+echo "Nettoyage des sources..."
 
-# On zip le tout
+# Suppression des fichiers inutiles
+If ( Test-Path "$tmp\Resources\source.zip"                     ) { Remove-Item "$tmp\Resources\source.zip"                     }
+If ( Test-Path "$tmp\Gestionnaire de Fond d'Écran.exe"         ) { Remove-Item "$tmp\Gestionnaire de Fond d'Écran.exe"         }
+If ( Test-Path "$tmp\Gestionnaire de Fond d'Écran.csproj.user" ) { Remove-Item "$tmp\Gestionnaire de Fond d'Écran.csproj.user" }
+
+# Suppression des dossiers inutiles
+If ( Test-Path "$tmp\.vs"  ) { Remove-Item "$tmp\.vs" -Recurse }
+If ( Test-Path "$tmp\bin"  ) { Remove-Item "$tmp\bin" -Recurse }
+If ( Test-Path "$tmp\obj"  ) { Remove-Item "$tmp\obj" -Recurse }
+If ( Test-Path "$tmp\tmp"  ) { Remove-Item "$tmp\tmp" -Recurse }
+
+echo "Archivage des sources..."
+
+# On zip les sources dans le dossier de sortie
 Add-Type -assembly "System.IO.Compression.Filesystem"
 [IO.Compression.ZipFile]::CreateFromDirectory($tmp, $output)
 
-echo "L'archive Zip des sources a été créée !"
+echo "L'archive des sources a été créée à l'emplacement : $output !"
 
 # On supprime le dossier temporaire
 Remove-Item $tmp -Recurse
